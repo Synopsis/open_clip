57e33429fc77ff9a58b37792b9ca2daf66233976
Rahul Somani <rsomani95@gmail.com>
2022-12-13 02:31:25 -0500

    explicitly use square images

--- src/open_clip/transform.py
+++ src/open_clip/transform.py
@@ -63,7 +63,7 @@ def image_transform(
     normalize = Normalize(mean=mean, std=std)
     if is_train:
         return Compose([
-            Resize(image_size, interpolation=InterpolationMode.BICUBIC, antialias=True),
+            Resize((image_size, image_size), interpolation=InterpolationMode.BICUBIC, antialias=True),
             # RandomResizedCrop(image_size, scale=(0.9, 1.0), interpolation=InterpolationMode.BICUBIC),
             _convert_to_rgb,
             ToTensor(),
