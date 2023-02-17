0c66110d1b89d5c079807eb47174d5bef0122fee
Rahul Somani <rsomani95@gmail.com>
2023-02-17 03:58:42 -0500

    * ability to use without finetuned ckpt * auto create save filename

--- src/compute_image_embeddings.py
+++ src/compute_image_embeddings.py
@@ -27,18 +27,19 @@ if True:
 
 from cinemanet.CLIP.inference import compute_image_embeddings
 from cinemanet.CLIP.utils import interpolate_weights
-from cinemanet.CLIP.utils import load_model
+from cinemanet.CLIP.utils import load_model, load_interpolated_model
 
 
 @call_parse
 def run_embeddings(
     variant:       P("Model arch", str) = "ViT-L-14",
     pretrained:    P("Pretrained?", str) = "openai",
-    ckpt_path:     P("", str) = None,
-    img_folders:   P("", str, nargs="+") = ["/home/synopsis/datasets/shotdeck-thumbs/"],
-    img_files:     P("", str, nargs="+") = None,
-    alpha:         P("Alpha to blend `pretrained` and `ckpt_path` model", float) = 0.5,
-    experiment:    P("Name of the experiment", str) = None,
+    ckpt_path:     P("Path to the checkpoint. If `None`, the stock model is used", str) = None,
+    img_folders:   P("(Optional) Folders with images to analyse", str, nargs="+") = ["/home/synopsis/datasets/shotdeck-thumbs/"],
+    img_files:     P("(Optional) List of image files to analyses", str, nargs="+") = None,
+    img_files_json: P("(Optional) A JSON file that contains a list of filepaths to analyse") = "/home/synopsis/git/CinemaNet-Training/assets/shotdeck_sample_100k.json",
+    alpha:         P("If using `ckpt_path`, alpha value to blend the pretrained & finetuned model", float) = None,
+    suffix:        P("Suffix to add to .feather filename", str) = "shotdeck_sample_embeddings",
     batch_size:    P("Batch Size", int) = 32,
     num_workers:   P("DataLoader num workers", int) = 4,
     device:        P("Device", int) = 0,
@@ -57,23 +58,39 @@ def run_embeddings(
     if not save_dir:
         raise RuntimeError(f"You need to enter a `save_dir`")
 
-    if not experiment:
-        raise RuntimeError(f"Require `experiment` name to determine the save filename")
-
     save_dir = Path(save_dir)
     save_dir.mkdir(exist_ok=True)
-    save_path_df = save_dir / f"{experiment}.feather"
-    save_path_json = save_dir / f"{experiment}_args.json"
+    filename = f"{variant}--{pretrained}"
+    if ckpt_path:
+        filename = f"{filename}--finetuned-alpha-{str(alpha)}"
+    filename = f"{filename}__{suffix}"
+    save_path_df = save_dir / f"{filename}.feather"
+    save_path_json = save_dir / f"{filename}_args.json"
 
-    model = load_model(variant, device, None, ckpt_path)
-    model.eval()
+    if ckpt_path is not None:
+        model = load_interpolated_model(variant, device, pretrained, ckpt_path, alpha=alpha)
+        rich.print(f"Loaded interpolated weights at Alpha={alpha}")
+    else:
+        model = load_model(variant, device, pretrained, ckpt_path)
+        rich.print(f"Loaded pretrained model")
 
-    sd_stock = torch.load("/home/synopsis/git/open_clip/src/openai-ViT-L-14_pretrained.pth", map_location="cpu")
-    sd_finetune = {k:v.detach().cpu() for k,v in model.state_dict().items()}
-    interpolated_wts = interpolate_weights(sd_finetune, sd_stock, alpha)
+    # model = load_model(variant, device, None, ckpt_path)
+    # model.eval()
 
-    model.load_state_dict(interpolated_wts)
-    rich.print(f"Loaded interpolated weights at Alpha={alpha}")
+    # sd_stock = torch.load("/home/synopsis/git/open_clip/src/openai-ViT-L-14_pretrained.pth", map_location="cpu")
+    # sd_finetune = {k:v.detach().cpu() for k,v in model.state_dict().items()}
+    # interpolated_wts = interpolate_weights(sd_finetune, sd_stock, alpha)
+
+    # model.load_state_dict(interpolated_wts)
+    # rich.print(f"Loaded interpolated weights at Alpha={alpha}")
+
+    if img_files_json is not None:
+        if img_folders or img_files:
+            logger.warning(
+                f"Ignoring `img_files` and `img_folders` and using `img_files_json` to select files to be analysed"
+            )
+            img_folders = None
+            img_files = load_json(img_files_json)
 
     df = compute_image_embeddings(model, img_files, img_folders, batch_size, num_workers)
     df.to_feather(save_path_df)
