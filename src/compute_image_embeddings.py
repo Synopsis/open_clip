3efc30f551f071195ba57caef8bd181f2ea44f18
Rahul Somani <rsomani95@gmail.com>
2023-02-24 09:52:40 -0500

    refactor to use new inference class

--- src/compute_image_embeddings.py
+++ src/compute_image_embeddings.py
@@ -3,7 +3,7 @@ import torch
 
 from upyog.all import *
 from upyog.cli import Param as P
-
+from cinemanet.CLIP.inference import EVALUATION_PROMPTS
 
 if torch.cuda.is_available():
     # This enables tf32 on Ampere GPUs which is only 8% slower than
@@ -25,64 +25,38 @@ if True:
     sys.path.append("/home/synopsis/git/cinemanet-multitask-classification/")
     sys.path.append("/home/synopsis/git/Synopsis.py/")
 
-from cinemanet.CLIP.inference import compute_image_embeddings
-from cinemanet.CLIP.utils import interpolate_weights
-from cinemanet.CLIP.utils import load_model, load_interpolated_model
+from training.inference import InferenceModelFromDisk
 
 
 @call_parse
 def run_embeddings(
-    variant:       P("Model arch", str) = "ViT-L-14",
+    arch:          P("Model arch", str) = "ViT-L-14",
     pretrained:    P("Pretrained?", str) = "openai",
     ckpt_path:     P("Path to the checkpoint. If `None`, the stock model is used", str) = None,
     img_folders:   P("(Optional) Folders with images to analyse", str, nargs="+") = ["/home/synopsis/datasets/shotdeck-thumbs/"],
     img_files:     P("(Optional) List of image files to analyses", str, nargs="+") = None,
-    img_files_json: P("(Optional) A JSON file that contains a list of filepaths to analyse") = "/home/synopsis/git/CinemaNet-Training/assets/shotdeck_sample_100k.json",
+    img_files_json: P("(Optional) A JSON file that contains a list of filepaths to analyse") = "/home/synopsis/git/CinemaNet-Training/assets/shotdeck_sample_830k.json",
     alpha:         P("If using `ckpt_path`, alpha value to blend the pretrained & finetuned model", float) = None,
-    suffix:        P("Suffix to add to .feather filename", str) = "shotdeck_sample_embeddings",
+    exp_name:      P("Name of the experiment. Acts as suffix to add to .feather filename", str) = "shotdeck_sample_embeddings",
     batch_size:    P("Batch Size", int) = 32,
     num_workers:   P("DataLoader num workers", int) = 4,
     device:        P("Device", int) = 0,
     save_dir:      P("Path to save the DataFrame to", str) = "/home/synopsis/datasets/serialised-datasets/CLIP/",
-) -> Tuple[pd.DataFrame, dict]:
+) -> Tuple[InferenceModelFromDisk, pd.DataFrame]:
+
     args = deepcopy(locals())
     rich.print(f"\nArgs:\n{args}\n")
 
-    if pretrained and ckpt_path:
-        if alpha is None:
-            raise RuntimeError(
-                f"You provided a pretrained model & checkpoint path to blend, but did not "
-                f"provide alpha values to do the blending"
-            )
-
-    if not save_dir:
-        raise RuntimeError(f"You need to enter a `save_dir`")
-
-    save_dir = Path(save_dir)
-    save_dir.mkdir(exist_ok=True)
-    filename = f"{variant}--{pretrained}"
-    if ckpt_path:
-        filename = f"{filename}--finetuned-alpha-{str(alpha)}"
-    filename = f"{filename}__{suffix}"
-    save_path_df = save_dir / f"{filename}.feather"
-    save_path_json = save_dir / f"{filename}_args.json"
-
-    if ckpt_path is not None:
-        model = load_interpolated_model(variant, device, pretrained, ckpt_path, alpha=alpha)
-        rich.print(f"Loaded interpolated weights at Alpha={alpha}")
-    else:
-        model = load_model(variant, device, pretrained, ckpt_path)
-        rich.print(f"Loaded pretrained model")
-
-    # model = load_model(variant, device, None, ckpt_path)
-    # model.eval()
-
-    # sd_stock = torch.load("/home/synopsis/git/open_clip/src/openai-ViT-L-14_pretrained.pth", map_location="cpu")
-    # sd_finetune = {k:v.detach().cpu() for k,v in model.state_dict().items()}
-    # interpolated_wts = interpolate_weights(sd_finetune, sd_stock, alpha)
-
-    # model.load_state_dict(interpolated_wts)
-    # rich.print(f"Loaded interpolated weights at Alpha={alpha}")
+    inf = InferenceModelFromDisk(
+                   arch = arch,
+                 device = device,
+                  alpha = alpha,
+             pretrained = pretrained,
+              ckpt_path = ckpt_path,
+         path_embedding = None,
+        experiment_name = exp_name,
+    )
+    rich.print(f"Model Info:\n{inf}\n")
 
     if img_files_json is not None:
         if img_folders or img_files:
@@ -92,12 +66,11 @@ def run_embeddings(
             img_folders = None
             img_files = load_json(img_files_json)
 
-    df = compute_image_embeddings(model, img_files, img_folders, batch_size, num_workers)
-    df.to_feather(save_path_df)
+    df = inf.get_image_embeddings(img_files, img_folders, batch_size, num_workers, save_dir)
 
+    save_path_json = str(inf.path_embedding).replace(".feather", ".json")
     with open(save_path_json, "w") as f:
         json.dump(args, f, indent=4)
-    rich.print(f"Wrote cached embeddings as DataFrame to {save_path_df}")
     rich.print(f"Wrote accompanying metadata as JSON to {save_path_json}")
 
-    return df, args
+    return inf, df
