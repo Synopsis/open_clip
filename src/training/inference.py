f2b6fd8f8b6590fca917a2b4ea843f1ce0907ff8
Rahul Somani <rsomani95@gmail.com>
2023-05-09 13:10:08 -0400

    - major refactor:     - stop saving state when not needed     - remove mixin bs - separate function to save embeddings df - better docstrings

--- src/training/inference.py
+++ src/training/inference.py
@@ -66,90 +66,19 @@ def _load_params_txt(file_path):
     return config_dict
 
 
-class InferenceMixin:
-    def eval_imagenet(
-        self,
-        path_imagenet = "/home/synopsis/datasets/ImageNet/validation/",
-        wandb_id = None,
-    ) -> dict:
-        """
-        Run inference on the ImageNet validation set using open-clip's prompts
-        """
-        from training.train import evaluate
-        from training.data import get_imagenet
-        from open_clip.factory import image_transform
-
-        args = self._get_eval_args(
-            self.batch_size, 4, path_imagenet, wandb_id, self.device)
-
-        imagenet_data = {}
-        image_mean = self.model.visual.image_mean
-        image_std = self.model.visual.image_std
-        preproc = image_transform(
-            self.model.visual.image_size, False, image_mean, image_std)
-        imagenet_data["imagenet-val"] = get_imagenet(args, (None, preproc), split="val")
-
-        return evaluate(self.model, imagenet_data, 1, args)
-
-    def eval_cinemanet(self, categories: Optional[List[str]] = None) -> Tuple[dict, dict, dict]:
-        """
-        Run inference on the CinemaNet validation sets
-
-        Returns 3 dictionaries:
-            1. Dict[str, float]            -- overall accuracy by category
-            2. Dict[str, Dict[str, float]] -- accuracy per label per category
-            3. Dict[str, Dict[str, float]] -- inaccuracy per label per category
-        """
-        accuracies_overall = {}
-        accuracies_per_label = {}
-        inaccuracies_per_label = {}
-        confusion_matrices = {}
-
-        categories = categories or sorted(TAXONOMY.keys())
-        for category in categories:
-            if self.arch.endswith("-custom-text"):
-                taxonomy         = {category: TAXONOMY[category]}
-                reverse_taxonomy = {category: REVERSE_TAXONOMY[category]}
-            else:
-                taxonomy         = {category: TAXONOMY_CUSTOM_TOKENS[category]}
-                reverse_taxonomy = {category: REVERSE_TAXONOMY_CUSTOM_TOKENS[category]}
-
-            # TODO: Log confusion matrix
-            acc,_,_,acc_per_label,inacc_per_label,confusion_matrix = run_cinemanet_eval(
-                self.model, self.tokenizer, category, batch_size=self.batch_size,
-                verbose=False, taxonomy=taxonomy, reverse_taxonomy=reverse_taxonomy,
-            )
-            accuracies_overall[category] = acc
-            accuracies_per_label[category] = acc_per_label
-            inaccuracies_per_label[category] = inacc_per_label
-            confusion_matrices[category] = confusion_matrix
-
-        return accuracies_overall, accuracies_per_label, inaccuracies_per_label, confusion_matrices
-
-
-class InferenceModel(InferenceMixin):
-
+class InferenceModel:
     def __init__(
         self,
         arch: str,
-        device: Union[str, int, torch.device],
-        alpha: Optional[float],
         pretrained: str,
-        ckpt_path: Optional[str],
-        batch_size: int = 8,
-        # ...
-        path_embedding: Optional[PathLike] = None,
-        experiment_name: str = None,
+        device: Union[str, int, torch.device],
+        ckpt_path: Optional[str] = None,
+        alpha: Optional[float] = None,
+        batch_size: int = 128,
     ):
         """
-        Helper class to run inference easily for:
-        1) Pretrained models
-        2) Trained model that can be blended with a pretrained model
-
-        Some key methods:
-        `self.eval_cinemanet()`
-        `self.eval_imagenet()`
-        `self.get_image_embeddings()`
+        Helper class to easily run inference for both pretrained and/or trained checkpoints.
+        If loading a trained checkpoint, you can dynamically reset the `alpha` to interpolate weights on the fly
 
         ARGS:
             arch:       Name of the the architecture. Must be one of `open_clip.list_models()`
@@ -158,6 +87,11 @@ class InferenceModel(InferenceMixin):
             alpha:      (Optional) used in conjunction with `ckpt_path` to interpolate the checkpoint with pretrained model
             ckpt_path:  (Optional) Path to a saved `.ckpt` file to load the weights from. If not provided, a pretrained model is loaded
             batch_size: Inference batch size
+
+        Some key methods:
+            `self.eval_cinemanet()`
+            `self.eval_imagenet()`
+            `self.get_image_embeddings()`
         """
         self.arch = arch
         self.device = device
@@ -165,11 +99,6 @@ class InferenceModel(InferenceMixin):
         self.pretrained = pretrained
         self.ckpt_path = ckpt_path
         self.batch_size = batch_size
-
-        # FIXME: Why the fuck are we taking `path_embedding` and `experiment_name`. That logic should be outside of this class??
-        self.path_embedding = path_embedding
-        assert experiment_name, f"`experiment_name` required"
-        self.experiment_name = experiment_name
 
         self._check_args()
         self.model = self.load_model(self.alpha)
@@ -188,7 +117,8 @@ class InferenceModel(InferenceMixin):
         pretrained = cfg["pretrained"]
 
         return cls(
-            arch, device, alpha, pretrained, ckpt_path, batch_size, path_embedding, experiment_name
+            arch=arch, device=device, alpha=alpha, pretrained=pretrained, ckpt_path=ckpt_path,
+            batch_size=batch_size, path_embedding=path_embedding, experiment_name=experiment_name,
         )
 
     @property
@@ -232,21 +162,6 @@ class InferenceModel(InferenceMixin):
     def load_tokenizer(self):
         return get_tokenizer(self.arch)
 
-    def _create_embedding_filename(
-        self,
-        suffix: str,
-        save_dir = "/home/synopsis/datasets/serialised-datasets/CLIP/"
-    ) -> Path:
-
-        save_dir = Path(save_dir)
-        save_dir.mkdir(exist_ok=True, parents=True)
-        filename = f"{self.arch}--{self.pretrained}"
-        if self.ckpt_path:
-            filename = f"{filename}--finetuned-alpha-{str(self.alpha)}"
-        filename = f"{filename}__{suffix}.feather"
-
-        return save_dir / filename
-
     def _get_eval_args(self, batch_size, num_workers, path_imagenet, wandb_id, device):
         return SimpleNamespace(
             # Model
@@ -273,40 +188,105 @@ class InferenceModel(InferenceMixin):
         img_folders: Optional[List[PathLike]] = None,
         batch_size: Optional[int] = 16,
         num_workers: int = 4,
-        save_dir: Optional[PathLike] = "/home/synopsis/datasets/serialised-datasets/CLIP/CLIP-Embeddings-Cached/",
     ) -> pd.DataFrame:
         """
         If we have passed `path_embeddings` on init, reads the cached embeddings and returns them
         Else, we compute embeddings, save them to disk, and return them
         """
-        if self.path_embedding is not None:
-            logger.info(f"Reading pre-computed embeddings")
-            return pd.read_feather(self.path_embedding)
-
         if img_files is None and img_folders is None:
             raise RuntimeError(f"Enter `img_files` and/or `img_folders` to point to images that must be analysed")
 
-        df = compute_image_embeddings(
+        return compute_image_embeddings(
             self.model, img_files, img_folders, batch_size or self.batch_size, num_workers)
 
-        if save_dir:
-            save_dir = Path(save_dir)
-            save_path = self._create_embedding_filename(self.experiment_name, save_dir)
-            df.to_feather(save_path)
-            self.path_embedding = save_path
-            logger.success(f"Saved cached embeddings to {save_path}")
-        else:
-            logger.warning(f"No 'save_dir' given. Embeddings are not being cached!")
+    def _create_save_embedding_filepath(
+        self,
+        suffix: Optional[str] = None,
+        save_dir = "/home/synopsis/datasets/serialised-datasets/CLIP/"
+    ) -> Path:
 
-        return df
+        save_dir = Path(save_dir)
+        save_dir.mkdir(exist_ok=True, parents=True)
+        filename = f"{self.arch}--{self.pretrained}"
+        if self.ckpt_path:
+            filename = f"{filename}--finetuned-alpha-{str(self.alpha)}"
 
-    def eval_cinemanet(self) -> Tuple[dict, dict, dict]:
-        self.cnet_acc, self.cnet_acc_per_label, self.cnet_inacc_per_label = super().eval_cinemanet()
-        return self.cnet_acc, self.cnet_acc_per_label, self.cnet_inacc_per_label
+        if suffix: filename = f"{filename}__{suffix}.feather"
+        else:      filename = f"{filename}.feather"
 
-    def eval_imagenet(self, path_imagenet="/home/synopsis/datasets/ImageNet/validation/", wandb_id=None) -> dict:
-        self.imgnet_metrics = super().eval_imagenet(path_imagenet, wandb_id)
-        return self.imgnet_metrics
+        return save_dir / filename
+
+    def save_embedding_df(
+        self,
+        df: pd.DataFrame,
+        suffix: Optional[str] = None,
+        save_dir = "/home/synopsis/datasets/serialised-datasets/CLIP/CLIP-Embeddings-Cached/"
+    ) -> PathLike:
+        save_path = self._create_save_embedding_filepath(suffix, save_dir)
+        df.to_feather(save_path)
+        logger.success(f"Saved cached embeddings to {save_path}")
+
+        return save_path
+
+    def eval_imagenet(
+        self,
+        path_imagenet = "/home/synopsis/datasets/ImageNet/validation/",
+        wandb_id = None,
+    ) -> Dict[str, float]:
+        """
+        Run inference on the ImageNet validation set using open-clip's prompts
+        """
+        from training.train import evaluate
+        from training.data import get_imagenet
+        from open_clip.factory import image_transform
+
+        args = self._get_eval_args(
+            self.batch_size, 4, path_imagenet, wandb_id, self.device)
+
+        imagenet_data = {}
+        image_mean = self.model.visual.image_mean
+        image_std = self.model.visual.image_std
+        preproc = image_transform(
+            self.model.visual.image_size, False, image_mean, image_std)
+        imagenet_data["imagenet-val"] = get_imagenet(args, (None, preproc), split="val")
+
+        return evaluate(self.model, imagenet_data, 1, args)
+
+    def eval_cinemanet(self, categories: Optional[List[str]] = None) -> Tuple[dict, dict, dict, dict]:
+        """
+        Run inference on the CinemaNet validation sets
+
+        Returns 4 dictionaries:
+            1. Dict[str, float]            -- overall accuracy by category
+            2. Dict[str, Dict[str, float]] -- accuracy per label per category
+            3. Dict[str, Dict[str, float]] -- inaccuracy per label per category
+            4. Dict[str, mpl.Figure]       -- confusion matrices per category
+        """
+        accuracies_overall = {}
+        accuracies_per_label = {}
+        inaccuracies_per_label = {}
+        confusion_matrices = {}
+
+        categories = categories or sorted(TAXONOMY.keys())
+        for category in categories:
+            if self.arch.endswith("-custom-text"):
+                taxonomy         = {category: TAXONOMY[category]}
+                reverse_taxonomy = {category: REVERSE_TAXONOMY[category]}
+            else:
+                taxonomy         = {category: TAXONOMY_CUSTOM_TOKENS[category]}
+                reverse_taxonomy = {category: REVERSE_TAXONOMY_CUSTOM_TOKENS[category]}
+
+            # TODO: Log confusion matrix
+            acc,_,_,acc_per_label,inacc_per_label,confusion_matrix = run_cinemanet_eval(
+                self.model, self.tokenizer, category, batch_size=self.batch_size,
+                verbose=False, taxonomy=taxonomy, reverse_taxonomy=reverse_taxonomy,
+            )
+            accuracies_overall[category] = acc
+            accuracies_per_label[category] = acc_per_label
+            inaccuracies_per_label[category] = inacc_per_label
+            confusion_matrices[category] = confusion_matrix
+
+        return accuracies_overall, accuracies_per_label, inaccuracies_per_label, confusion_matrices
 
     def __repr__(self) -> str:
         path_embed = f"'{self.path_embedding}'" if self.path_embedding else None
@@ -325,7 +305,7 @@ class InferenceModel(InferenceMixin):
 def unwrap_model(model):
     return model.module if hasattr(model, 'module') else model
 
-class InferenceModelWhileTraining(InferenceMixin):
+class InferenceModelWhileTraining(InferenceModel):
     def __init__(self, model, tokenizer, orig_state_dict, args, alpha):
         self.model = unwrap_model(model)
         self.model.device = torch.device(args.device)
@@ -363,12 +343,5 @@ class InferenceModelWhileTraining(InferenceMixin):
     def _get_eval_args(self, *args, **kwargs):
         return self.args
 
-    def get_image_embeddings(
-        self,
-        img_files: Optional[List[PathLike]] = None,
-        img_folders: Optional[List[PathLike]] = None,
-        batch_size: Optional[int] = 16,
-        num_workers: int = 4,
-    ):
-        return compute_image_embeddings(
-            self.model, img_files, img_folders, batch_size, num_workers, "Computing ShotDeck Embeddings...")
+    def __repr__(self) -> str:
+        return f"InferenceModelWhileTraining(...)"
